1b124256efb477cb935d209e4198a977e81adae4
Petr Veselý <Petr.Vesel@email.cz>
2020-04-03 12:21:27 +0200

    Přidejte soubory projektu.

---
+++ 31. Přepona OPP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31.Přepona_OPP
+{
+    private int Num1;
+
+    class EnterTheNumbers
+    {
+        Console.Write("Zadejte délky odvěsen pravoúhlého trojúhelníku: ");
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
